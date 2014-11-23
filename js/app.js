5341ba2d8b4aa1d7c35acabd7c0cab2bc5541ce1
Kyle Stevenson <kyle@kylestevenson.me>
2014-11-23 14:12:31 -0800

    encapsulate models

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 var resumeApp = angular.module('resumeApp', []);
 
 resumeApp.controller('PersonController', function ($scope) {
-	$scope.me = {
+	var self = {
 		name: "Kyle Stevenson",
 		major: "Computer Science",
 		year: "Sophomore",
@@ -15,16 +15,22 @@ resumeApp.controller('PersonController', function ($scope) {
 		}
 	};
 
-	$scope.socialIcons = [
+	var socialIcons = [
 		{ src: "img/icons/octocat.svg",  href: "https://github.com/kylestev" },
 		{ src: "img/icons/linkedin.png", href: "http://linkedin.com/in/kylestev" },
 		{ src: "img/icons/twitter.svg",  href: "https://twitter.com/kylestev" },
 		{ src: "img/icons/gplus.svg",    href: "https://plus.google.com/+KyleStevenson-kylestev/posts" }
 	];
+
+	$scope.me = self;
+
+	$scope.getSocialIcons = function () {
+		return socialIcons;
+	};
 });
 
 resumeApp.controller('JobController', function ($scope) {
-	$scope.jobs = [
+	var jobs = [
 		{
 			company: { name: "Rackspace", color: "red" },
 			dates: { start: "June 2014" },
@@ -78,17 +84,21 @@ resumeApp.controller('JobController', function ($scope) {
 		}
 	];
 
+	$scope.getJobs = function () {
+		return jobs;
+	};
+
 	$scope.printDates = function (job) {
 		if ( ! ("end" in job.dates)) {
 			return job.dates.start + " - Present";
 		}
 
 		return job.dates.start + " - " + job.dates.end;
-	}
+	};
 });
 
 resumeApp.controller('ProjectController', function ($scope) {
-	$scope.projects = [
+	var projects = [
 		{
 			title: "Which Course For Me",
 			description: "Which Course for Me is a web application providing current, consumable information about course information at Oregon State University. Conveniently, there are interfaces for both humans (Bootstrap + AngularJS), and for machines (JSON API)!",
@@ -107,4 +117,8 @@ resumeApp.controller('ProjectController', function ($scope) {
 			repo: "https://github.com/kylestev/jbcm"
 		}
 	];
+
+	$scope.getProjects = function () {
+		return projects;
+	};
 });
